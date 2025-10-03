5645694a8eef3ededca82158976aac22d3417048
Borja-68 <borjapazpar@gmail.com>
2025-10-03 18:42:49 +0200

    añadidos proyectos de interfaces

---
+++ diseño interfazes/arrastraventanas/src/main/java/com/mycompany/arrastraventanas/Arrastraventanas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arrastraventanas;
+
+/**
+ *
+ * @author a24borjapp
+ */
+public class Arrastraventanas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ diseño interfazes/projectoAgenda/src/main/java/com/mycompany/projectoagenda/ProjectoAgenda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectoagenda;
+
+/**
+ *
+ * @author a24borjapp
+ */
+public class ProjectoAgenda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
