2dd89ae1295c38b5fff0f6ea23d6fa0294579e1c
Borja-68 <borjapazpar@gmail.com>
2025-10-03 19:44:44 +0200

    añadidos proyectos de interfaces

---
+++ diseño interfazes/Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author a24borjapp
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+    }
+}
