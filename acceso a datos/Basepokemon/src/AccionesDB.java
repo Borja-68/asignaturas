d492f98bc6e6b0e53341fc11761e6155b2b29ce4
Borja-68 <borjapazpar@gmail.com>
2025-10-08 20:20:51 +0200

    multiples ejercicios de acceso a datos

--- acceso a datos/Basepokemon/src/AccionesDB.java
+++ acceso a datos/Basepokemon/src/AccionesDB.java
@@ -1,2 +1,42 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class AccionesDB {
+    public AccionesDB(){}
+
+    public static int Insercion(Connection conexion, Scanner input){
+        String nombre="";
+        String tipo="";
+        Integer nivel=-1;
+        do{
+            System.out.println("introduce el nombre del pokemon");
+            nombre=input.next();
+            input.nextLine();
+        }while(nombre.isEmpty());
+
+        do{
+            System.out.println("introduce el tipo/os del pokemon");
+            tipo=input.next();
+            input.nextLine();
+        }while(tipo.isEmpty());
+
+        do{
+            try {
+                System.out.println("introduce el nivel del pokemon, numero");
+                nivel = input.nextInt();
+                input.nextLine();
+            }catch (InputMismatchException e){
+                System.out.println("no se introdujo un numero");
+            }
+        }while(nivel<=0 || nivel>100);
+    try{
+        PreparedStatement sentencia= conexion.prepareStatement("INSERT INTO objetos.Pokemons (pokemon) VALUES (ROW('Pikachu', 'Eléctrico', 30);");
+    }catch (SQLException e){
+        System.out.println("insercion fallida");
+    }
+    return 0;
+    }
 }
