06461486617a83f3b797c6e00d6668675b144c4f
Borja-68 <borjapazpar@gmail.com>
2025-10-13 21:06:28 +0200

    update proyecto accceso a datos

--- acceso a datos/BaseDatosHospitales/src/ConexionesDB.java
+++ acceso a datos/BaseDatosHospitales/src/ConexionesDB.java
@@ -1,2 +1,48 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionesDB {
+
+    private static Connection conectionPostgre;
+    private String urlPostgre = "jdbc:postgresql://localhost:5432/hospital_postgre";
+    private static  String usuarioPostgre="postgres";
+
+    private static Connection conectionMysql;
+    private String urlMysql = "jdbc:mysql://localhost:3306/hospital_mysql";
+    private static  String usuarioMysql="root";
+
+    private static  String contrasena="abc123.";
+
+    private ConexionesDB() {
+        try {
+            conectionMysql = DriverManager.getConnection(urlMysql, usuarioMysql, contrasena);
+            conectionPostgre = DriverManager.getConnection(urlPostgre, usuarioPostgre, contrasena);
+        } catch (SQLException e) {
+            System.out.println("hubo un error en la creacion");
+        }
+    }
+
+    public static Connection getConectionMysql() {
+        if (conectionMysql == null) {
+            new ConexionesDB();
+        }
+        return conectionMysql;
+    }
+
+    public static Connection getConectionPostgre() {
+        if (conectionPostgre == null) {
+            new ConexionesDB();
+        }
+        return conectionPostgre;
+    }
+
+    public static void close() {
+        try {
+            conectionMysql.close();
+            conectionPostgre.close();
+        } catch (SQLException e) {
+            System.out.println("sucedio algo");
+        }
+    }
 }
