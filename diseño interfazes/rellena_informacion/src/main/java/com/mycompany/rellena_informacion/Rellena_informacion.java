c04b76bc68c82c176f9c80c89d2d58540675ef37
Borja-68 <borjapazpar@gmail.com>
2025-10-15 16:44:41 +0200

    nuevo proyecto interfaces

---
+++ diseño interfazes/rellena_informacion/src/main/java/com/mycompany/rellena_informacion/Rellena_informacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rellena_informacion;
+
+/**
+ *
+ * @author a24borjapp
+ */
+public class Rellena_informacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
