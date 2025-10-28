a0b3f42323053ae6fcef4b40fc8cc67748693cb7
Borja-68 <borjapazpar@gmail.com>
2025-10-28 20:56:29 +0100

    cosa de interfaces

---
+++ diseño interfazes/Prueba1/src/main/java/com/mycompany/prueba1/PrincipalController.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.prueba1;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author a24borjapp
+ */
+public class PrincipalController implements Initializable {
+
+    @FXML
+    private Button btsEnviar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void switchFinal(ActionEvent event) throws IOException{
+        App.setRoot("final");
+    }
+    
+}
